21cc662b14ff901ac75ce1ece133b92b935828f2
Brian Gebel <bgebel@gofundme.com>
2023-07-17 13:25:34 -0700

    update comments

--- src/data/persons.data.ts
+++ src/data/persons.data.ts
@@ -15,7 +15,7 @@ export const Persons: Person[] = [
     email: "test+001@gofundme.com",
     firstName: "Frodo",
     lastName: "Baggins",
-    role: PersonRole.PERSON, // TODO how do we make this dynamic? IE look up roles and specify them by campaign??? Solve this first before implementing policy... I think we need a PersonService. 
+    role: PersonRole.PERSON, // @todo Resolve this from a PersonService interface as it is not in our DB
     status: PersonStatus.ACTIVE
   },
   {
