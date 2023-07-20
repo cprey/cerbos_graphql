d43ecd1fdce83ebb63a139f2a6d4fcc9a794f9ce
Brian Gebel <bgebel@gofundme.com>
2023-07-20 14:37:56 -0700

    more updates

--- src/services/Authorization.service.ts
+++ src/services/Authorization.service.ts
@@ -4,8 +4,6 @@ import { Service } from "typedi";
 
 import { GRPC as Cerbos } from "@cerbos/grpc";
 import logger from "../utils/logger";
-import { AuthChecker } from "type-graphql";
-import { IContext } from "../server/context.interface";
 
 const log = logger("CerbosService");
 
@@ -24,15 +22,3 @@ export class AuthorizationService {
     this.cerbos = new Cerbos(config.cerbos.host, { tls: config.cerbos.tls });
   }
 }
-
-// as a class https://typegraphql.com/docs/authorization.html#how-to-use
-export const customAuthChecker: AuthChecker<IContext> = (
-  { root, args, context, info },
-  roles,
-) => {
-  // here we can read the user from context
-  // and check his permission in the db against the `roles` argument
-  // that comes from the `@Authorized` decorator, eg. ["ADMIN", "MODERATOR"]
-
-  return true; // or false if access is denied
-};
